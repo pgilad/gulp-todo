7890e2597e447b856cdb446ea1793ebe01151d3b
Gilad Peleg <giladp007@gmail.com>
2014-11-13 00:05:18 +0200

    Redo parser logic, add coffee support.
    Closes #24

--- tests/parse-spec.js
+++ tests/parse-spec.js
@@ -11,7 +11,8 @@ var getFixturePath = function (file) {
 
 var getComments = function (file) {
     var content = fs.readFileSync(file, 'utf8');
-    return parsers[path.extname(file)]()(content);
+    var ext = path.extname(file);
+    return parsers[ext]()(content);
 };
 
 describe('gulp-todo parsing', function () {
@@ -22,6 +23,8 @@ describe('gulp-todo parsing', function () {
             should.exist(comments);
             comments.should.have.length(1);
             comments[0].kind.should.equal('FIXME');
+            comments[0].line.should.equal(4);
+            comments[0].text.should.equal('use fixmes as well');
         });
 
         it('parse block line comments', function () {
@@ -30,7 +33,11 @@ describe('gulp-todo parsing', function () {
             should.exist(comments);
             comments.should.have.length(2);
             comments[0].kind.should.equal('TODO');
+            comments[0].line.should.equal(5);
+            comments[0].text.should.equal('single line comment with a todo');
             comments[1].kind.should.equal('FIXME');
+            comments[1].line.should.equal(6);
+            comments[1].text.should.equal('single line comment with a todo');
         });
     });
 
@@ -41,9 +48,17 @@ describe('gulp-todo parsing', function () {
             should.exist(comments);
             comments.should.have.length(4);
             comments[0].kind.should.equal('TODO');
+            comments[0].line.should.equal(2);
+            comments[0].text.should.equal('only output this author names if an author exists');
             comments[1].kind.should.equal('FIXME');
+            comments[1].line.should.equal(8);
+            comments[1].text.should.equal('This comment will not be in the output');
             comments[2].kind.should.equal('TODO');
+            comments[2].text.should.equal('Multiple line comment');
+            comments[2].line.should.equal(13);
             comments[3].kind.should.equal('TODO');
+            comments[3].line.should.equal(13);
+            comments[3].text.should.equal('and again');
         });
     });
 
@@ -54,12 +69,16 @@ describe('gulp-todo parsing', function () {
             should.exist(comments);
             comments.should.have.length(4);
             comments[0].kind.should.equal('TODO');
+            comments[0].text.should.equal('it will appear in the CSS output.');
             comments[0].line.should.equal(2);
             comments[1].kind.should.equal('FIXME');
+            comments[1].text.should.equal('this is a block comment too');
             comments[1].line.should.equal(3);
             comments[2].kind.should.equal('FIXME');
+            comments[2].text.should.equal('They won\'t appear in the CSS output,');
             comments[2].line.should.equal(10);
             comments[3].kind.should.equal('TODO');
+            comments[3].text.should.equal('improve this syntax');
             comments[3].line.should.equal(14);
         });
     });
@@ -71,6 +90,7 @@ describe('gulp-todo parsing', function () {
             should.exist(comments);
             comments.should.have.length(1);
             comments[0].kind.should.equal('TODO');
+            comments[0].text.should.equal('add another class');
             comments[0].line.should.equal(4);
         });
     });
@@ -83,8 +103,10 @@ describe('gulp-todo parsing', function () {
             comments.should.have.length(2);
             comments[0].kind.should.equal('TODO');
             comments[0].line.should.equal(1);
+            comments[0].text.should.equal('change to public');
             comments[1].kind.should.equal('FIXME');
             comments[1].line.should.equal(11);
+            comments[1].text.should.equal('use jquery');
         });
     });
 
@@ -96,7 +118,22 @@ describe('gulp-todo parsing', function () {
             comments.should.have.length(1);
             comments[0].kind.should.equal('TODO');
             comments[0].line.should.equal(14);
-            comments[0].value.should.equal('Show my TODO please');
+            comments[0].text.should.equal('Show my TODO please');
+        });
+    });
+
+    describe('coffeescript', function () {
+        it('handle # comments', function () {
+            var file = getFixturePath('coffee.coffee');
+            var comments = getComments(file);
+            should.exist(comments);
+            comments.should.have.length(2);
+            comments[0].kind.should.equal('TODO');
+            comments[0].line.should.equal(1);
+            comments[0].text.should.equal('Do something');
+            comments[1].kind.should.equal('FIXME');
+            comments[1].line.should.equal(3);
+            comments[1].text.should.equal('Fix something');
         });
     });
 });
