5b17f07d9d91e490093ad7187afc69e8e8898cfe
Gilad Peleg <giladp007@gmail.com>
2014-11-26 11:16:06 +0200

    add example of gulp todo json

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,33 @@
 'use strict';
 var gulp = require('gulp');
 var todo = require('./index');
+var wrap = require('gulp-wrap');
+
+gulp.task('json', function () {
+    gulp.src('./**/*.js', {
+        base: './'
+    })
+        .pipe(todo({
+            fileName: 'TODO.json',
+            newLine: ',\n',
+            transformHeader: function () {
+                return '';
+            },
+            transformComment: function (file, line, text, kind) {
+                return ['{' + '"file": "' + file.replace(/"/g, '\\"') + '"',
+                    '"text": "' + text.replace(/"/g, '\\"') + '"',
+                    '"kind": "' + kind + '"',
+                    '"line": ' + line + '}'];
+            }
+        }))
+        .pipe(wrap('[\n<%= contents %>\n]'))
+        .pipe(gulp.dest('./'));
+});
 
 gulp.task('default', function () {
-    gulp.src('index.js')
+    gulp.src(['./index.js', './lib/**.*.js'], {
+        base: './'
+    })
         .pipe(todo())
         .pipe(gulp.dest('./'));
 });
