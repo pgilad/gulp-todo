0856de24b7ceb0ada35b3eb0bca37196a7af2438
Gilad Peleg <giladp007@gmail.com>
2014-12-03 22:28:23 +0200

    add example xml in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,28 @@
 'use strict';
 var gulp = require('gulp');
 var todo = require('./index');
+var wrap = require('gulp-wrap');
+
+gulp.task('xml', function () {
+    gulp.src('./**/*.js', {
+        base: './'
+    })
+        .pipe(todo({
+            fileName: 'todo.xml',
+            reporter: 'default',
+            transformComment: function (file, line, text, kind) {
+                return ['<comment file="' + file + '" line="' + line + '" pattern="' + kind + '">',
+                    text, '</comment>'];
+                //jshint unused:false
+            },
+            transformHeader: function () {
+                return '';
+            }
+        }))
+        .pipe(wrap('<comments xmlns="http://todos.sourceforge.net" version="0.1.0"> <%= contents %></comments>'))
+        .pipe(gulp.dest('./'));
+});
+
 
 gulp.task('json', function () {
     gulp.src('./**/*.js', {
